62d877b7c4a46d335e7a3e7f4147723b294210db
92hyungjun <92hyungjun@gmail.com>
2021-01-29 15:55:42 +0900

    bug fix

--- func.c
+++ func.c
@@ -1,16 +1,20 @@
 
 
+
+//준열
 int sum( int a, int b ) {
     // commit 1
     return a + b;
 }
 
+//동빈
 int sub( int a, int b ) {
     // commit 2
     return a - b;
 }
 
-int mul( int* a, int b ) {
+//형준
+int mul( int a, int b ) {
     // commit 3
     return a * b;
 }
